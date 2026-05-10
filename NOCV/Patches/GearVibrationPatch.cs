3cb5187a37d3bb34b5045137129c982598bed76e
pauel <pau.lacazedieu@gmail.com>
2026-05-10 19:51:30 +0200

    Fixed null checking of aircraft: caused errors in console and bad perf.

--- NOCV/Patches/GearVibrationPatch.cs
+++ NOCV/Patches/GearVibrationPatch.cs
@@ -95,6 +95,7 @@ public class GearVibrationPatch: VibChannelUser<GearVibrationPatch>
 
     private static AudioSource CheckAndCallStartPlay(AudioSource source, int motorIndex, float maxMagnitude, LandingGear gear)
     {
+        if (gear.aircraft is null) return source;
         return !GameManager.IsLocalAircraft(gear.aircraft) ? source : VibOnAudioSources.StartPlaying(source, motorIndex, maxMagnitude);
     }
     
